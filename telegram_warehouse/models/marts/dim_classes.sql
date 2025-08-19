9f35369eedbe9db625e6680bce3ef904742ce187
tsega b <tsegabogale92@gmail.com>
2025-08-19 14:05:38 +0300

    readme modification

--- telegram_warehouse/models/marts/dim_classes.sql
+++ telegram_warehouse/models/marts/dim_classes.sql
@@ -1,7 +1,11 @@
--- models/marts/dim_classes.sql
+{{ config(
+    materialized='table',
+    schema='analytics'
+) }}
 
 SELECT DISTINCT
     LOWER(TRIM(detected_class)) AS class_name,
-    MD5(LOWER(TRIM(detected_class))) AS class_id -- You can also use surrogate int ids if preferred
+    MD5(LOWER(TRIM(detected_class))) AS class_id  -- Or use surrogate int IDs if preferred
 FROM {{ ref('stg_image_detections') }}
 WHERE detected_class IS NOT NULL
+
--- telegram_warehouse/models/marts/fct_image_detection.sql
+++ telegram_warehouse/models/marts/fct_image_detection.sql
@@ -1,12 +1,31 @@
+{{ config(
+    materialized='table',
+    schema='raw_analytics'
+) }}
+
+WITH image_detections_clean AS (
+    SELECT
+        d.detection_id,
+        d.image_id,
+        LOWER(TRIM(d.detected_class)) AS detected_class,  -- corrected column name
+        d.confidence_score
+    FROM {{ ref('stg_image_detections') }} d
+    WHERE d.detected_class IS NOT NULL
+),
+
+image_info AS (
+    SELECT
+        i.image_id,
+        i.image_date
+    FROM {{ ref('stg_telegram_images') }} i
+)
+
 SELECT
     d.detection_id,
     d.image_id,
-    c.class_id,
+    d.detected_class,
     d.confidence_score,
-     
     i.image_date
-FROM {{ ref('stg_image_detections') }} d
-LEFT JOIN {{ ref('dim_classes') }} c
-  ON LOWER(TRIM(d.detected_class)) = c.class_name
-LEFT JOIN {{ ref('stg_telegram_images') }} i
-  ON d.image_id = i.image_id
+FROM image_detections_clean d
+LEFT JOIN image_info i
+    ON d.image_id = i.image_id
